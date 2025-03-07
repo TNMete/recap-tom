4b3979c0a858302798a2c58170ce72a0cc4778af
TNMete <mete.karaoglan@tn.techstarter.de>
2025-03-07 09:07:00 +0100

    x

--- gitdocu.js
+++ gitdocu.js
@@ -1,6 +1,7 @@
 const fetchGitWorkflowImage = async () => {
-  const imagePath = "";
-  const gitDocuContainer = undefined;
+  const imagePath = "./images/git/Git_Workflow.png";
+  const gitDocuContainer = document.getElementById("gitDocuContainer");
   // Image in den gitDocuContainer einfüge (TIPP: .innerHTML)
   // Das Image sol einen vordefinierten Style hinterlegt
+  gitDocuContainer.innerHTML = `<img class="gitDocuImg" src="${imagePath}" alt="git-Documentation" />`;
 };
